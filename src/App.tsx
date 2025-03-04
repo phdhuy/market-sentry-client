6334619948c3c9bb56569a0bacd06d46f4eca0f1
phduchuy <phanhoangduchuyy@gmail.com>
2025-03-04 23:44:07 +0700

    refactor: refactor code router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import Sidebar from "./components/Sidebar/Sidebar";
-import MarketPage from "./pages/market/MarketPage";
 import { ThemeProvider } from "next-themes";
-import ProfilePage from "./pages/setting/profile/ProfilePage";
-import WatchlistPage from "./pages/watchlist/WatchlistPage";
-import NotificationPage from "./pages/notification/NotificationPage";
-import AccountSecurityPage from "./pages/setting/security/AccountSecurityPage";
-import AlertPage from "./pages/alert/AlertPage";
-import PortfolioPage from "./pages/portfolio/PortfolioPage";
-import { Routes, Route } from "react-router-dom";
+import AppRoutes from "./routers/router";
 
 export default function App() {
   return (
@@ -22,16 +15,7 @@ export default function App() {
         >
           <Sidebar />
           <main className="p-6">
-            <Routes>
-              <Route path="/" element={<PortfolioPage />} />
-              <Route path="/market" element={<MarketPage />} />
-              <Route path="/alert" element={<AlertPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/watchlist" element={<WatchlistPage />} />
-              <Route path="/account-security" element={<AccountSecurityPage />} />
-              <Route path="/notification" element={<NotificationPage />} />
-              <Route path="*" element={<PortfolioPage />} /> {/* Fallback Route */}
-            </Routes>
+            <AppRoutes />
           </main>
         </div>
       </div>
